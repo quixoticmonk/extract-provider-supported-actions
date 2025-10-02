c1e2aaccf3174c82607158ceec86b64b84f00336
Manu Chandrasekhar <manuchn@amazon.com>
2025-10-02 15:22:38 -0400

    fix: main.tf file

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+    tfe = {
+      source = "hashicorp/tfe"
+    }
+    awscc = {
+      source = "hashicorp/awscc"
+    }
+  }
+}
